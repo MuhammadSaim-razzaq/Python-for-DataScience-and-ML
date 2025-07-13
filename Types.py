10521627b39c1448fa80a556d579144ec12114e7
MuhammadSaim-razzaq <saimrazzaq1103@gmail.com>
2025-07-13 13:10:43 -0700

    Create Types.py

---
+++ Types.py
@@ -0,0 +1,14 @@
+print(type('12'))
+print(type(12))
+
+import sys
+print(sys.float_info)
+
+print(float(0))
+print(float(3.3))
+print(str(1))
+print(int('1234345356456646'))
+print(type(6//2))
+print(6//2)
+print(type("hello" == "world"))
+print(int("1001"))
