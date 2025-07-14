318b3d17dc5474092538e91cdef03ba338b920f6
MuhammadSaim-razzaq <saimrazzaq1103@gmail.com>
2025-07-14 06:23:26 -0700

    Create details.py

---
+++ details.py
@@ -0,0 +1,10 @@
+name= input("What's your name: ")
+color = input("what's your favouite color: ")
+print(name + " like "+ color)
+
+birth_year= input("Enter birth year: ")
+print(2025- int(birth_year))
+
+weight_lbs = input('Enter wieght in lbs: ')
+weight_kg = float(weight_lbs)*0.45
+print(weight_kg)
